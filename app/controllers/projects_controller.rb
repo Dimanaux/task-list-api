4281e5def7dd9facaab6634a547adfed7502a9d7
Dmitry Barskov <dmitry.barskov64@gmail.com>
2020-06-20 00:47:55 +0300

    controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,36 @@
 class ProjectsController < ApplicationController
+  expose :projects, ->{ Project.all }
+  expose :project
+
   def index
-    render json: { message: 'ok' }
+  end
+
+  def show
+  end
+
+  def create
+    if project.save
+      redirect_to project_path(project)
+    else
+      render json: project.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if project.update(project_params)
+      redirect_to project_path(project)
+    else
+      render json: project.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    project.destroy
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title)
   end
 end
