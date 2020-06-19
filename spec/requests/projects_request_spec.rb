fbfd05e445a7c16bdd5e173e244bed62ceedd4ef
Dmitry Barskov <dmitry.barskov64@gmail.com>
2020-06-20 00:48:02 +0300

    spec support

--- spec/requests/projects_request_spec.rb
+++ spec/requests/projects_request_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe 'Projects', type: :request do
-  describe 'GET #index' do
-    before { get '/projects' }
+RSpec.describe 'projects', type: :request do
+  include_context "with json response"
 
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
+  let(:project) { create(:project, title: 'Conquer the world') }
+  let(:todo) { create(:todo, text: 'Start with default city', project: project) }
+
+  describe 'GET /index' do
+    let(:fixture_path) { 'projects_index.json' }
+    let(:fixture_params) { { project_id: project.id, todo_id: todo.id } }
+
+    before do
+      todo
+      get projects_url
     end
 
-    it 'returns ok' do
-      json_response = JSON.parse(response.body)
-      expect(json_response).to eq('message' => 'ok')
+    it 'renders projects with todos' do
+      expect(response).to have_http_status(:success)
+      expect(body).to match(fixture)
     end
   end
 end
---
+++ spec/support/shared_contexts/with_json_response.rb
@@ -0,0 +1,16 @@
+RSpec.shared_context "with json response" do
+  let(:body) { JSON.parse(response.body) }
+
+  let(:raw_fixture) do
+    File.read(File.join(RSpec.configuration.fixture_path, fixture_path))
+  end
+
+  let(:fixture) do
+    JSON.parse(
+      raw_fixture.gsub(
+        /#{fixture_params.keys.map { |s| ':' + s.to_s }.join('|')}/,
+        fixture_params.map { |k, v| [':' + k.to_s, v] }.to_h
+      )
+    )
+  end
+end
